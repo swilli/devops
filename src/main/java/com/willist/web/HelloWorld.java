1c5eeae2ce2c00300443692117778c9f2b26b443
Stefan Willi <stefan.j.willi@gmail.com>
2018-09-25 00:18:20 +0200

    Added first implementation.

---
+++ src/main/java/com/willist/web/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.willist.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    private static int num = 0;
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index() {
+        num += 1;
+        return "replying to GET request # " + String.valueOf(num) + "...";
+    }
+
+}
